1c788c4843c58ff931915e34b83eb19b64ee1478
ousui <mi-nato@outlook.com>
2024-02-05 22:29:37 +0900

    commit3

--- LOL_start.py
+++ LOL_start.py
@@ -1,20 +1,40 @@
 import asyncio
 import willump
 import time
+import requests
 import sys
 import os
+from requests.packages import urllib3
 
-#获取lol信息
+#状态
+Found = "Found" 
+Invalid = "Invalid" 
+Searching = "Searching"
+searchStateURL = "/lol-lobby/v2/lobby/matchmaking/search-state"
+foundacceptURL = "/lol-matchmaking/v1/ready-check/accept"
+ingameURL = "https://127.0.0.1:2999/liveclientdata/eventdata?eventID=0"
+
+#队列信息 t1
 async def get_summoner_data():
-    summoner = await (await wllp.request("GET", "/lol-summoner/v1/current-summoner")).json()
-    state = await (await wllp.request("GET", "/lol-lobby/v2/lobby/matchmaking/search-state")).json()
-    print(f"summonerName:    {summoner['displayName']}")
-    print(f"summonerLevel:   {summoner['summonerLevel']}")
-    print(f"searchState:     {state['searchState']}")
-    print(f"---")
+    state = await (await wllp.request("GET", searchStateURL)).json()
     return state['searchState']
 
-#检测现在队列状态
+#点击确定
+async def Found():
+    global wllp
+    wllp = await willump.start()
+    await wllp.request('POST', foundacceptURL)
+    await wllp.close()
+
+#游戏内信息 t2
+def ingamecheck():
+    try:
+        urllib3.disable_warnings()
+        state=requests.get(ingameURL,verify=False,timeout=1.000)
+        return(200)
+    except BaseException:
+        return(0)
+
 async def main():
     global wllp
     wllp = await willump.start()
@@ -22,35 +42,23 @@ async def main():
     await wllp.close()
     return t1
 
-#匹配到人后点击确定
-async def Found():
-    global wllp
-    wllp = await willump.start()
-    await wllp.request('POST', '/lol-matchmaking/v1/ready-check/accept')
-    await wllp.close()
-
 #启动
-def search():
+if __name__ == '__main__':
     while True:
         t1 = asyncio.run(main())
-        print(t1)
-        if t1 == 'Found':
+        t2 = ingamecheck()
+        if t2 == 200:
+            os._exit(0)
+            sys.exit()
+        elif t1 == Found:
             asyncio.run(Found())
             time.sleep(10)
-            t1 = asyncio.run(main())
-            if t1 == 'Invalid':
-                os._exit(0)
-                sys.exit()
-            else:
-                continue
-        elif t1 == 'Invalid':
+            continue
+        elif t1 == Invalid:
             time.sleep(2)
             continue
-        elif t1 == 'Searching':
+        elif t1 == Searching:
             time.sleep(1)
             continue
         else:
             break
-
-if __name__ == '__main__':
-    search()
